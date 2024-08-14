56b0571ef09fd57feaa3e0cea923b62121ccc2db
adotterer <adotterer@gmail.com>
2024-08-14 12:05:43 -0500

    Prettier setup

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  projects: [
+    {
+      displayName: "browser",
+      preset: "@marko/jest/preset/browser",
+      testMatch: ["**/__tests__/**/*.browser.js"],
+    },
+  ],
+};
---
+++ src/components/__tests__/mouse-mask.browser.js
@@ -0,0 +1,3 @@
+test(    "contains the text", async () => {
+  throw Error("hello from test");
+});
